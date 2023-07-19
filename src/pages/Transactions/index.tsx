19512184cb8950703d2b2ad6c448e49f7b6547aa
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-07-18 22:22:55 -0300

    Summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header"
+import { Summary } from "../../components/Header/Summary/Index"
 
 export function Transactions (){
     return (
         <div>
             <Header/>
+            <Summary />
         </div>
     )
 }
