e6b0060ac3a53e470cf414a4e3d1f914f47566b4
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-07-21 22:09:59 -0300

    Axios configuration

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useState } from "react";
+import { api } from "../lib/axios";
 
 interface Transaction {
   id: number;
@@ -24,18 +25,13 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   async function fetchTransactions(query?: string) {
-    const url = new URL("http://localhost:3333/transactions");
-
-    if (query) {
-      url.searchParams.append("q", query);
-    }
-
-    const response = await fetch(url);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const data = await response.json();
-
+    const response = await api.get("/transactions",{
+      params: {
+        q: query
+      }
+    });
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    setTransactions(data);
+    setTransactions(response.data);
   }
   useEffect(() => {
     void fetchTransactions();
